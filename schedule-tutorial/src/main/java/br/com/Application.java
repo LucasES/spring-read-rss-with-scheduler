3732c9cdc920d65bde1a8ebc2aab081f0e4d0c94
lucas.araujo <lucas.araujo@ifactory.com.br>
2016-10-19 16:59:10 -0300

    Adicionando configuração spring boot para suportar deploy war

--- schedule-tutorial/src/main/java/br/com/Application.java
+++ schedule-tutorial/src/main/java/br/com/Application.java
@@ -2,23 +2,30 @@ package br.com;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 import br.com.scheduler.ScheduledTasks;
 
+@Configuration
 @SpringBootApplication
 @EnableScheduling
-@PropertySource({
-	"classpath:scheduler.properties",
-})
-public class Application {
+@PropertySource({ "classpath:scheduler.properties", })
+public class Application extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Application.class);
+	}
+
+	public static void main(String[] args) throws Exception {
 		SpringApplication.run(Application.class, args);
 	}
-	
+
 	@Bean
 	public ScheduledTasks getScheduledTasks() {
 		return new ScheduledTasks();
