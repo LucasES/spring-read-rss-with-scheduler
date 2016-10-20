98e6445ad233a50bed816923a1d26b0995b490f4
lucas.araujo <lucas.araujo@ifactory.com.br>
2016-10-20 11:34:58 -0300

    Resolvendo bug de scheduler ser invocado duas vezes.

--- schedule-tutorial/src/main/java/br/com/Application.java
+++ schedule-tutorial/src/main/java/br/com/Application.java
@@ -4,13 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
-
-import br.com.scheduler.ScheduledTasks;
 
 @Configuration
 @SpringBootApplication
@@ -26,10 +23,5 @@ public class Application extends SpringBootServletInitializer {
 
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run(Application.class, args);
-	}
-
-	@Bean
-	public ScheduledTasks getScheduledTasks() {
-		return new ScheduledTasks();
 	}
 }
--- schedule-tutorial/src/main/java/br/com/scheduler/ScheduledTasks.java
+++ schedule-tutorial/src/main/java/br/com/scheduler/ScheduledTasks.java
@@ -1,8 +1,5 @@
 package br.com.scheduler;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,14 +15,11 @@ public class ScheduledTasks {
 	private static final String url = "http://www.valor.com.br/brasil/infraestrutura/rss"; 
     private static final Logger log = LoggerFactory.getLogger(ScheduledTasks.class);
 
-    private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
-    
     @Autowired
     private IRssService service;
 
     @Scheduled(cron = "${scheduling.job}")
     public void reportCurrentTime() {
-        log.info("The time is now {}", dateFormat.format(new Date()));
     	RestTemplate restTemplate = new RestTemplate();
     	Rss myRss = restTemplate.getForObject(url, Rss.class);
     	if(myRss != null) {
